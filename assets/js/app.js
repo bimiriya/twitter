3d518eaada178fcd2e3d3d230c0f5e6bd9de2ed6
bimiriya <poyarzunaspee@gmail.com>
2017-11-07 14:30:20 -0300

    task 2 completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,15 @@
+function contar() { 
+    var max = "140"; 
+    var cadena = document.getElementById("new-twt-content").value; 
+    var longitud = cadena.length; 
+
+        if(longitud <= max) { 
+             document.getElementById("contador").value = max-longitud; 
+        } else { 
+             document.getElementById("new-twt-content").value = cadena.substr(0, max);
+        } 
+};
+
 function add() {
     var twtNewContent = document.getElementById("new-twt-content");
     var twtBtn = document.getElementById("twt-btn");
@@ -14,4 +26,12 @@ function add() {
     newTwt.appendChild(dateSpan)
     dateSpan.textContent = new Date();
     newContent.textContent = twtNewContent.value;
+    timeLine.insertBefore(newTwt, timeLine.children[0]);
 };
+
+var disabledBtn = document.getElementById("twt-btn").disabled = true;
+
+function enableBtn() {
+   var blueBtn = document.getElementById("twt-btn").removeAttribute("disabled");  
+}
+
