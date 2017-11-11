840e7a24da1c00ae0a626a0981f6f8303a2062c2
bimiriya <poyarzunaspee@gmail.com>
2017-11-11 19:29:32 -0300

    disabled button + color change

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+//botón para twitear desabilitado por default
+var disabledBtn = document.getElementById("twt-btn").disabled = true;
+
 //función para contar caracteres en reversa 
 //y cambiar el color del contador
 function contar() { 
@@ -5,7 +8,11 @@ function contar() {
     var cadena = document.getElementById("new-twt-content").value; 
     var longitud = cadena.length; 
 
-        if(longitud <= max) { 
+        if (longitud >= 1) { //para abilitar botón
+            var blueBtn = document.getElementById("twt-btn").removeAttribute("disabled"); 
+            document.getElementById("twt-btn").style.backgroundColor = "rgb(102, 204, 218)";
+            document.getElementById("twt-btn").style.color = "white";
+        } else if(longitud <= max) { 
              document.getElementById("contador").value = max-longitud; 
         } else if (longitud > 0) { //cuando el contador llegue a 0 (puesto que empieza en 140 hacia atrás)
             document.getElementById("contador").value = "-" + (longitud - max); //agregará un - más la longitud del texto a la cual se le restan los 140 caracteres
@@ -28,23 +35,25 @@ textarea.oninput = function() {
 
 //función para imprimir tweet en el timeline
 function add() {
-    var twtNewContent = document.getElementById("new-twt-content");
-    var twtBtn = document.getElementById("twt-btn");
-    var timeLine = document.getElementById("timeline");
+        var twtNewContent = document.getElementById("new-twt-content");
+        var twtBtn = document.getElementById("twt-btn");
+        var timeLine = document.getElementById("timeline");
+        
+        var newTwt = document.createElement("div");
+        var newContent = document.createElement("p");
+        var dateSpan = document.createElement("span");
+        var getHours = new Date().getHours();
+        var getMinutes = new Date().getMinutes();
+        var getTime = getHours + ":" + getMinutes;
     
-    var newTwt = document.createElement("div");
-    var newContent = document.createElement("p");
-    var dateSpan = document.createElement("span");
-    var getHours = new Date().getHours();
-    var getMinutes = new Date().getMinutes();
-    var getTime = getHours + ":" + getMinutes;
-
-    newTwt.className = "new-twt";
-
-    timeLine.appendChild(newTwt)
-    newTwt.appendChild(newContent);
-    newTwt.appendChild(dateSpan);
-    dateSpan.textContent = getTime;
-    newContent.textContent = twtNewContent.value;
-    timeLine.insertBefore(newTwt, timeLine.children[0]);
+        newTwt.className = "new-twt";
+    
+        timeLine.appendChild(newTwt)
+        newTwt.appendChild(newContent);
+        newTwt.appendChild(dateSpan);
+        dateSpan.textContent = getTime;
+        newContent.textContent = twtNewContent.value;
+        timeLine.insertBefore(newTwt, timeLine.children[0]);
+    
+        twtNewContent.value = "";
 };
